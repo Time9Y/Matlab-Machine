ee6c6313e0f3a986bfa5a51f2feee6048f291774
Time9Y <110336517+Time9Y@users.noreply.github.com>
2023-02-18 13:15:10 +0800

    Add files via upload

---
+++ 006_Time series forecasting based on support vector machine (libsvm)/main.m
@@ -0,0 +1,101 @@
+%%  清空环境变量
+warning off             % 关闭报警信息
+close all               % 关闭开启的图窗
+clear                   % 清空变量
+clc                     % 清空命令行
+
+%%  导入数据（时间序列的单列数据）
+result = xlsread('数据集.xlsx');
+
+%%  数据分析
+num_samples = length(result);  % 样本个数 
+kim = 15;                      % 延时步长（kim个历史数据作为自变量）
+zim =  1;                      % 跨zim个时间点进行预测
+
+%%  构造数据集
+for i = 1: num_samples - kim - zim + 1
+    res(i, :) = [reshape(result(i: i + kim - 1), 1, kim), result(i + kim + zim - 1)];
+end
+
+%%  划分训练集和测试集
+temp = 1: 1: 922;
+
+P_train = res(temp(1: 700), 1: 15)';
+T_train = res(temp(1: 700), 16)';
+M = size(P_train, 2);
+
+P_test = res(temp(701: end), 1: 15)';
+T_test = res(temp(701: end), 16)';
+N = size(P_test, 2);
+
+%%  数据归一化
+[p_train, ps_input] = mapminmax(P_train, 0, 1);
+p_test = mapminmax('apply', P_test, ps_input);
+
+[t_train, ps_output] = mapminmax(T_train, 0, 1);
+t_test = mapminmax('apply', T_test, ps_output);
+
+%%  转置以适应模型
+p_train = p_train'; p_test = p_test';
+t_train = t_train'; t_test = t_test';
+
+%%  创建模型
+c = 4.0;    % 惩罚因子
+g = 0.8;    % 径向基函数参数
+cmd = [' -t 2',' -c ',num2str(c),' -g ',num2str(g),' -s 3 -p 0.01'];
+model = svmtrain(t_train, p_train, cmd);
+
+%%  仿真预测
+[t_sim1, error_1] = svmpredict(t_train, p_train, model);
+[t_sim2, error_2] = svmpredict(t_test , p_test , model);
+
+%%  数据反归一化
+T_sim1 = mapminmax('reverse', t_sim1, ps_output);
+T_sim2 = mapminmax('reverse', t_sim2, ps_output);
+
+%%  均方根误差
+error1 = sqrt(sum((T_sim1' - T_train).^2) ./ M);
+error2 = sqrt(sum((T_sim2' - T_test ).^2) ./ N);
+
+%%  绘图
+figure
+plot(1: M, T_train, 'r-', 1: M, T_sim1, 'b-', 'LineWidth', 1)
+legend('真实值','预测值')
+xlabel('预测样本')
+ylabel('预测结果')
+string = {'训练集预测结果对比'; ['RMSE=' num2str(error1)]};
+title(string)
+xlim([1, M])
+grid
+
+figure
+plot(1: N, T_test, 'r-', 1: N, T_sim2, 'b-', 'LineWidth', 1)
+legend('真实值','预测值')
+xlabel('预测样本')
+ylabel('预测结果')
+string = {'测试集预测结果对比';['RMSE=' num2str(error2)]};
+title(string)
+xlim([1, N])
+grid
+
+%%  相关指标计算
+%  R2
+R1 = 1 - norm(T_train - T_sim1')^2 / norm(T_train - mean(T_train))^2;
+R2 = 1 - norm(T_test -  T_sim2')^2 / norm(T_test -  mean(T_test ))^2;
+
+disp(['训练集数据的R2为：', num2str(R1)])
+disp(['测试集数据的R2为：', num2str(R2)])
+
+%  MAE
+mae1 = sum(abs(T_sim1' - T_train)) ./ M ;
+mae2 = sum(abs(T_sim2' - T_test )) ./ N ;
+
+disp(['训练集数据的MAE为：', num2str(mae1)])
+disp(['测试集数据的MAE为：', num2str(mae2)])
+
+%  MBE
+mbe1 = sum(T_sim1' - T_train) ./ M ;
+mbe2 = sum(T_sim2' - T_test ) ./ N ;
+
+disp(['训练集数据的MBE为：', num2str(mbe1)])
+disp(['测试集数据的MBE为：', num2str(mbe2)])
