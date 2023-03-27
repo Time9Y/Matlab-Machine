531eff1a815c7aa54ead802813a19d549066b2b0
Time9Y <110336517+Time9Y@users.noreply.github.com>
2023-03-27 23:13:10 +0800

    Add files via upload

---
+++ 024_Data regression prediction based on particle swarm optimization algorithm optimized BP neural network/fun.m
@@ -0,0 +1,28 @@
+function error = fun(pop, hiddennum, net, p_train, t_train)
+
+%%  节点个数
+inputnum  = size(p_train, 1);  % 输入层节点数
+outputnum = size(t_train, 1);  % 输出层节点数
+
+%%  提取权值和阈值
+w1 = pop(1 : inputnum * hiddennum);
+B1 = pop(inputnum * hiddennum + 1 : inputnum * hiddennum + hiddennum);
+w2 = pop(inputnum * hiddennum + hiddennum + 1 : ...
+    inputnum * hiddennum + hiddennum + hiddennum * outputnum);
+B2 = pop(inputnum * hiddennum + hiddennum + hiddennum * outputnum + 1 : ...
+    inputnum * hiddennum + hiddennum + hiddennum * outputnum + outputnum);
+ 
+%%  网络赋值
+net.Iw{1, 1} = reshape(w1, hiddennum, inputnum );
+net.Lw{2, 1} = reshape(w2, outputnum, hiddennum);
+net.b{1}     = reshape(B1, hiddennum, 1);
+net.b{2}     = B2';
+
+%%  网络训练
+net = train(net, p_train, t_train);
+
+%%  仿真测试
+t_sim1 = sim(net, p_train);
+
+%%  适应度值
+error = sum(sqrt(sum((t_sim1 - t_train) .^ 2) ./ length(t_sim1)));
