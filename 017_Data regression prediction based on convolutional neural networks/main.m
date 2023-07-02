6a3d97aefa6918f31883296b45c3758e2b1bc93b
Time9Y <110336517+Time9Y@users.noreply.github.com>
2023-07-02 16:07:56 +0800

    Add files via upload

--- 017_Data regression prediction based on convolutional neural networks/main.m
+++ 017_Data regression prediction based on convolutional neural networks/main.m
@@ -18,39 +18,48 @@ P_test = res(temp(81: end), 1: 7)';
 T_test = res(temp(81: end), 8)';
 N = size(P_test, 2);
 
+%%  数据归一化
+[p_train, ps_input] = mapminmax(P_train, 0, 1);
+p_test = mapminmax('apply', P_test, ps_input);
+
+[t_train, ps_output] = mapminmax(T_train, 0, 1);
+t_test = mapminmax('apply', T_test, ps_output);
+
 %%  数据平铺
 %   将数据平铺成1维数据只是一种处理方式
 %   也可以平铺成2维数据，以及3维数据，需要修改对应模型结构
 %   但是应该始终和输入层数据结构保持一致
-p_train =  double(reshape(P_train, 7, 1, 1, M));
-p_test  =  double(reshape(P_test , 7, 1, 1, N));
-t_train =  double(T_train)';
-t_test  =  double(T_test )';
+p_train =  double(reshape(p_train, 7, 1, 1, M));
+p_test  =  double(reshape(p_test , 7, 1, 1, N));
+t_train =  double(t_train)';
+t_test  =  double(t_test )';
 
 %%  构造网络结构
 layers = [
- imageInputLayer([7, 1, 1])      % 输入层 输入数据规模[7, 1, 1]
+ imageInputLayer([7, 1, 1])                         % 输入层 输入数据规模[7, 1, 1]
  
- convolution2dLayer([3, 1], 16)  % 卷积核大小 3*1 生成16张特征图
- batchNormalizationLayer         % 批归一化层
- reluLayer                       % Relu激活层
+ convolution2dLayer([3, 1], 16, 'Padding', 'same')  % 卷积核大小 3*1 生成16张特征图
+ batchNormalizationLayer                            % 批归一化层
+ reluLayer                                          % Relu激活层
  
- convolution2dLayer([3, 1], 32)  % 卷积核大小 3*1 生成32张特征图
- batchNormalizationLayer         % 批归一化层
- reluLayer                       % Relu激活层
+ maxPooling2dLayer([2, 1], 'Stride', [1, 1])        % 最大池化层 池化窗口 [2, 1] 步长 [1, 1]
 
- dropoutLayer(0.2)               % Dropout层
- fullyConnectedLayer(1)          % 全连接层
- regressionLayer];               % 回归层
+ convolution2dLayer([3, 1], 32, 'Padding', 'same')  % 卷积核大小 3*1 生成32张特征图
+ batchNormalizationLayer                            % 批归一化层
+ reluLayer                                          % Relu激活层
+
+ dropoutLayer(0.1)                                  % Dropout层
+ fullyConnectedLayer(1)                             % 全连接层
+ regressionLayer];                                  % 回归层
 
 %%  参数设置
 options = trainingOptions('sgdm', ...      % SGDM 梯度下降算法
-    'MiniBatchSize', 30, ...               % 批大小,每次训练样本个数30
-    'MaxEpochs', 800, ...                  % 最大训练次数 800
+    'MiniBatchSize', 32, ...               % 批大小,每次训练样本个数 32
+    'MaxEpochs', 1200, ...                 % 最大训练次数 1200
     'InitialLearnRate', 1e-2, ...          % 初始学习率为0.01
     'LearnRateSchedule', 'piecewise', ...  % 学习率下降
-    'LearnRateDropFactor', 0.5, ...        % 学习率下降因子
-    'LearnRateDropPeriod', 400, ...        % 经过400次训练后 学习率为 0.01 * 0.5
+    'LearnRateDropFactor', 0.1, ...        % 学习率下降因子
+    'LearnRateDropPeriod', 800, ...        % 经过 800 次训练后 学习率为 0.01 * 0.1
     'Shuffle', 'every-epoch', ...          % 每次训练打乱数据集
     'Plots', 'training-progress', ...      % 画出曲线
     'Verbose', false);
@@ -59,8 +68,12 @@ options = trainingOptions('sgdm', ...      % SGDM 梯度下降算法
 net = trainNetwork(p_train, t_train, layers, options);
 
 %%  模型预测
-T_sim1 = predict(net, p_train);
-T_sim2 = predict(net, p_test );
+t_sim1 = predict(net, p_train);
+t_sim2 = predict(net, p_test );
+
+%%  数据反归一化
+T_sim1 = mapminmax('reverse', t_sim1, ps_output);
+T_sim2 = mapminmax('reverse', t_sim2, ps_output);
 
 %%  均方根误差
 error1 = sqrt(sum((T_sim1' - T_train).^2) ./ M);
@@ -91,23 +104,47 @@ xlim([1, N])
 grid
 
 %%  相关指标计算
-%  R2
+% R2
 R1 = 1 - norm(T_train - T_sim1')^2 / norm(T_train - mean(T_train))^2;
 R2 = 1 - norm(T_test  - T_sim2')^2 / norm(T_test  - mean(T_test ))^2;
 
 disp(['训练集数据的R2为：', num2str(R1)])
 disp(['测试集数据的R2为：', num2str(R2)])
 
-%  MAE
+% MAE
 mae1 = sum(abs(T_sim1' - T_train)) ./ M ;
 mae2 = sum(abs(T_sim2' - T_test )) ./ N ;
 
 disp(['训练集数据的MAE为：', num2str(mae1)])
 disp(['测试集数据的MAE为：', num2str(mae2)])
 
-%  MBE
+% MBE
 mbe1 = sum(T_sim1' - T_train) ./ M ;
 mbe2 = sum(T_sim2' - T_test ) ./ N ;
 
 disp(['训练集数据的MBE为：', num2str(mbe1)])
 disp(['测试集数据的MBE为：', num2str(mbe2)])
+
+%%  绘制散点图
+sz = 25;
+c = 'b';
+
+figure
+scatter(T_train, T_sim1, sz, c)
+hold on
+plot(xlim, ylim, '--k')
+xlabel('训练集真实值');
+ylabel('训练集预测值');
+xlim([min(T_train) max(T_train)])
+ylim([min(T_sim1) max(T_sim1)])
+title('训练集预测值 vs. 训练集真实值')
+
+figure
+scatter(T_test, T_sim2, sz, c)
+hold on
+plot(xlim, ylim, '--k')
+xlabel('测试集真实值');
+ylabel('测试集预测值');
+xlim([min(T_test) max(T_test)])
+ylim([min(T_sim2) max(T_sim2)])
+title('测试集预测值 vs. 测试集真实值')
