ff882e7bf0ad0f529554b4b11773ab8371ffb717
Time9Y <110336517+Time9Y@users.noreply.github.com>
2023-07-04 13:34:39 +0800

    Add files via upload

--- 019_Time series forecasting based on convolutional neural networks/main.m
+++ 019_Time series forecasting based on convolutional neural networks/main.m
@@ -39,37 +39,35 @@ t_test = mapminmax('apply', T_test, ps_output);
 % 将数据平铺成1维数据只是一种处理方式
 % 也可以平铺成2维数据，以及3维数据，需要修改对应模型结构
 % 但是应该始终和输入层数据结构保持一致
-p_train = double(reshape(p_train, 15, 1, 1, M));
-p_test  = double(reshape(p_test , 15, 1, 1, N));
-t_train = double(t_train)';
-t_test  = double(t_test )';
+p_train =  double(reshape(p_train, 15, 1, 1, M));
+p_test  =  double(reshape(p_test , 15, 1, 1, N));
+t_train =  double(t_train)';
+t_test  =  double(t_test )';
 
 %%  构造网络结构
 layers = [
- imageInputLayer([15, 1, 1])     % 输入层 输入数据规模 [15, 1, 1]
+ imageInputLayer([15, 1, 1])                 % 输入层 输入数据规模[15, 1, 1]
  
- convolution2dLayer([3, 1], 16)  % 卷积核大小 3*1 生成16张特征图
- batchNormalizationLayer         % 批归一化层
- reluLayer                       % Relu激活层
+ convolution2dLayer([3, 1], 16, 'Stride', [1, 1], 'Padding', 'same')              
+                                             % 卷积核大小 3 * 1 生成 16 张特征图
+ batchNormalizationLayer                     % 批归一化层
+ reluLayer                                   % Relu激活层
  
- convolution2dLayer([3, 1], 32)  % 卷积核大小 3*1 生成32张特征图
- batchNormalizationLayer         % 批归一化层
- reluLayer                       % Relu激活层
+ convolution2dLayer([3, 1], 32, 'Stride', [1, 1], 'Padding', 'same') 
+                                             % 卷积核大小 3 * 1 生成 32 张特征图
+ batchNormalizationLayer                     % 批归一化层
+ reluLayer                                   % Relu激活层
 
- convolution2dLayer([3, 1], 32)  % 卷积核大小 3*1 生成32张特征图
- batchNormalizationLayer         % 批归一化层
- reluLayer                       % Relu激活层
-
- fullyConnectedLayer(1)          % 全连接层
- regressionLayer];               % 回归层
+ fullyConnectedLayer(1)                      % 全连接层
+ regressionLayer];                           % 回归层
 
 %%  参数设置
 options = trainingOptions('adam', ...      % Adam 梯度下降算法
-    'MaxEpochs', 300, ...                  % 最大训练次数 300
-    'InitialLearnRate', 1e-2, ...          % 初始学习率为0.01
+    'MaxEpochs', 800, ...                  % 最大训练次数 800
+    'InitialLearnRate', 5e-3, ...          % 初始学习率为 0.005
     'LearnRateSchedule', 'piecewise', ...  % 学习率下降
     'LearnRateDropFactor', 0.1, ...        % 学习率下降因子 0.1
-    'LearnRateDropPeriod', 200, ...        % 经过200次训练后 学习率为 0.01 * 0.1
+    'LearnRateDropPeriod', 600, ...        % 经过 600 次训练后 学习率为 0.005 * 0.1
     'Shuffle', 'every-epoch', ...          % 每次训练打乱数据集
     'Plots', 'training-progress', ...      % 画出曲线
     'Verbose', false);
@@ -114,23 +112,47 @@ xlim([1, N])
 grid
 
 %%  相关指标计算
-%  R2
+% R2
 R1 = 1 - norm(T_train - T_sim1')^2 / norm(T_train - mean(T_train))^2;
-R2 = 1 - norm(T_test -  T_sim2')^2 / norm(T_test -  mean(T_test ))^2;
+R2 = 1 - norm(T_test  - T_sim2')^2 / norm(T_test  - mean(T_test ))^2;
 
 disp(['训练集数据的R2为：', num2str(R1)])
 disp(['测试集数据的R2为：', num2str(R2)])
 
-%  MAE
+% MAE
 mae1 = sum(abs(T_sim1' - T_train)) ./ M ;
 mae2 = sum(abs(T_sim2' - T_test )) ./ N ;
 
 disp(['训练集数据的MAE为：', num2str(mae1)])
 disp(['测试集数据的MAE为：', num2str(mae2)])
 
-%  MBE
+% MBE
 mbe1 = sum(T_sim1' - T_train) ./ M ;
 mbe2 = sum(T_sim2' - T_test ) ./ N ;
 
 disp(['训练集数据的MBE为：', num2str(mbe1)])
 disp(['测试集数据的MBE为：', num2str(mbe2)])
+
+%%  绘制散点图
+sz = 25;
+c = 'b';
+
+figure
+scatter(T_train, T_sim1, sz, c)
+hold on
+plot(xlim, ylim, '--k')
+xlabel('训练集真实值');
+ylabel('训练集预测值');
+xlim([min(T_train) max(T_train)])
+ylim([min(T_sim1) max(T_sim1)])
+title('训练集预测值 vs. 训练集真实值')
+
+figure
+scatter(T_test, T_sim2, sz, c)
+hold on
+plot(xlim, ylim, '--k')
+xlabel('测试集真实值');
+ylabel('测试集预测值');
+xlim([min(T_test) max(T_test)])
+ylim([min(T_sim2) max(T_sim2)])
+title('测试集预测值 vs. 测试集真实值')
