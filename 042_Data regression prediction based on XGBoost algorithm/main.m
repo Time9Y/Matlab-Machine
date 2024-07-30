448cd7fa3e046490f86c645ba4686058753d52b7
Time9Y <110336517+Time9Y@users.noreply.github.com>
2024-07-30 14:32:39 +0800

    Add files via upload

---
+++ 042_Data regression prediction based on XGBoost algorithm/main.m
@@ -0,0 +1,118 @@
+%%  清空环境变量
+warning off             % 关闭报警信息
+close all               % 关闭开启的图窗
+clear                   % 清空变量
+clc                     % 清空命令行
+
+%%  导入数据
+res = xlsread('数据集.xlsx');
+
+%%  划分训练集和测试集
+temp = randperm(103);
+
+P_train = res(temp(1: 80), 1: 7)';
+T_train = res(temp(1: 80), 8)';
+M = size(P_train, 2);
+
+P_test = res(temp(81: end), 1: 7)';
+T_test = res(temp(81: end), 8)';
+N = size(P_test, 2);
+
+%%  数据归一化
+[p_train, ps_input] = mapminmax(P_train, 0, 1);
+p_test = mapminmax('apply', P_test, ps_input);
+
+[t_train, ps_output] = mapminmax(T_train, 0, 1);
+t_test = mapminmax('apply', T_test, ps_output);
+
+%%  数据转置
+p_train = p_train'; p_test = p_test';
+t_train = t_train'; t_test = t_test';
+ 
+%%  设置参数
+num_trees = 100;                  % 迭代次数
+params.eta = 0.1;                 % 学习率
+params.objective = 'reg:linear';  % 线性函数
+params.max_depth = 5;             % 最大深度
+
+%%  建立模型
+model = xgboost_train(p_train, t_train, params, num_trees);
+
+%%  预测
+t_sim1 = xgboost_test(p_train, model);
+t_sim2 = xgboost_test(p_test , model);
+
+%%  数据反归一化
+T_sim1 = mapminmax('reverse', t_sim1', ps_output);
+T_sim2 = mapminmax('reverse', t_sim2', ps_output);
+
+%%  均方根误差
+error1 = sqrt(sum((T_sim1 - T_train).^2) ./ M);
+error2 = sqrt(sum((T_sim2 - T_test ).^2) ./ N);
+
+%%  绘图
+figure
+plot(1: M, T_train, 'r-*', 1: M, T_sim1, 'b-o', 'LineWidth', 1)
+legend('真实值','预测值')
+xlabel('预测样本')
+ylabel('预测结果')
+string = {'训练集预测结果对比'; ['RMSE=' num2str(error1)]};
+title(string)
+xlim([1, M])
+grid
+
+figure
+plot(1: N, T_test, 'r-*', 1: N, T_sim2, 'b-o', 'LineWidth', 1)
+legend('真实值', '预测值')
+xlabel('预测样本')
+ylabel('预测结果')
+string = {'测试集预测结果对比'; ['RMSE=' num2str(error2)]};
+title(string)
+xlim([1, N])
+grid
+
+%%  相关指标计算
+% R2
+R1 = 1 - norm(T_train - T_sim1)^2 / norm(T_train - mean(T_train))^2;
+R2 = 1 - norm(T_test  - T_sim2)^2 / norm(T_test  - mean(T_test ))^2;
+
+disp(['训练集数据的R2为：', num2str(R1)])
+disp(['测试集数据的R2为：', num2str(R2)])
+
+% MAE
+mae1 = sum(abs(T_sim1 - T_train)) ./ M ;
+mae2 = sum(abs(T_sim2 - T_test )) ./ N ;
+
+disp(['训练集数据的MAE为：', num2str(mae1)])
+disp(['测试集数据的MAE为：', num2str(mae2)])
+
+% MBE
+mbe1 = sum(T_sim1 - T_train) ./ M ;
+mbe2 = sum(T_sim2 - T_test ) ./ N ;
+
+disp(['训练集数据的MBE为：', num2str(mbe1)])
+disp(['测试集数据的MBE为：', num2str(mbe2)])
+
+%%  绘制散点图
+sz = 25;
+c = 'b';
+
+figure
+scatter(T_train, T_sim1, sz, c)
+hold on
+plot(xlim, ylim, '--k')
+xlabel('训练集真实值');
+ylabel('训练集预测值');
+xlim([min(T_train) max(T_train)])
+ylim([min(T_sim1) max(T_sim1)])
+title('训练集预测值 vs. 训练集真实值')
+
+figure
+scatter(T_test, T_sim2, sz, c)
+hold on
+plot(xlim, ylim, '--k')
+xlabel('测试集真实值');
+ylabel('测试集预测值');
+xlim([min(T_test) max(T_test)])
+ylim([min(T_sim2) max(T_sim2)])
+title('测试集预测值 vs. 测试集真实值')
---
+++ 042_Data regression prediction based on XGBoost algorithm/xgboost_train.m
@@ -0,0 +1,80 @@
+function model = xgboost_train(p_train, t_train, params, max_num_iters)
+
+%%% Function inputs:
+% p_train:        matrix of inputs for the training set
+% t_train:        vetor of labels/values for the test set
+% params :        structure of learning parameters
+% max_num_iters: max number of iterations for learning
+
+%%% Function output:
+% model: a structure containing:
+%     iters_optimal; % number of iterations performs by xgboost (final model)
+%     h_booster_ptr; % pointer to the final model
+%     params;        % model parameters (just for info)
+%     missing;       % value considered "missing"
+
+%%  加载 xgboost 库
+loadlibrary('xgboost')
+
+%%  设置参数
+missing = single(NaN);          % 设置该值被视为"缺失"
+iters_optimal = max_num_iters;  % 最大迭代次数
+
+%%  设置xgboost的相关参数
+if isempty(params)
+    params.booster           = 'gbtree';
+    % params.objective         = 'binary:logistic';
+    params.objective         = 'reg:linear';
+    params.max_depth         = 5;
+    params.eta               = 0.1;
+    params.min_child_weight  = 1;
+    params.subsample         = 0.9;
+    params.colsample_bytree  = 1;
+    params.num_parallel_tree = 1;
+end
+
+%%  将属性转换为全局属性
+param_fields = fields(params);
+for i = 1 : length(param_fields)
+    eval(['params.' param_fields{i} ' = num2str(params.' param_fields{i} ');'])
+end
+
+%%  得到输入数据相关属性
+rows = uint64(size(p_train, 1));  % 输入数据的行 样本数
+cols = uint64(size(p_train, 2));  % 输入数据的列 特征数
+p_train = p_train';
+
+%%  创建相关指针
+p_train_ptr = libpointer('singlePtr', single(p_train));
+t_train_ptr = libpointer('singlePtr', single(t_train));
+
+h_train_ptr = libpointer;
+h_train_ptr_ptr = libpointer('voidPtrPtr', h_train_ptr);
+
+%%  处理输入特征
+calllib('xgboost', 'XGDMatrixCreateFromMat', p_train_ptr, rows, cols, missing, h_train_ptr_ptr);
+
+%%  处理标签
+labelStr = 'label';
+calllib('xgboost', 'XGDMatrixSetFloatInfo', h_train_ptr, labelStr, t_train_ptr, rows);
+
+%%  建立集成器并设置参数
+h_booster_ptr = libpointer;
+h_booster_ptr_ptr = libpointer('voidPtrPtr', h_booster_ptr);
+calllib('xgboost', 'XGBoosterCreate', h_train_ptr_ptr, uint64(1), h_booster_ptr_ptr);
+
+for i = 1 : length(param_fields)
+    eval(['calllib(''xgboost'', ''XGBoosterSetParam'', h_booster_ptr, ''' param_fields{i} ''', ''' eval(['params.' param_fields{i}]) ''');'])
+end
+
+%%  最终模型
+for iter = 0 : iters_optimal
+    calllib('xgboost', 'XGBoosterUpdateOneIter', h_booster_ptr, int32(iter), h_train_ptr);
+end
+
+%%  将模型参数保存到 model
+model                = struct;
+model.iters_optimal  = iters_optimal;  % 最大迭代次数
+model.h_booster_ptr  = h_booster_ptr;  % 指向最终模型的指针
+model.params         = params;         % 相关参数
+model.missing        = missing;        % 缺失值
